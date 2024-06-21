03e66e13a222659afd79b474b1452cf936f07fd0
reemagoud0928 <reemagoud09@gmail.com>
2024-06-21 09:14:43 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,17 @@
+class Add {
+    public static void main(String[] args) {
+        int a,b,c;
+        a=45;
+        b=32;
+        c=a+b;
+        System.out.println(c);
+         System.out.println("sum="+c);
+          System.out.println("value of a="+a+ " value of b="+b+ " sum ="+c);
+        }
+}
+
+
+
+
+
+
